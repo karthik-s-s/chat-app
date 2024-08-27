1e61b93b9bd6227241497d63fa03aea983ee8b6a
Karthik Sudhakar <karthiksudhakar999@gmail.com>
2024-08-27 15:13:43 +0530

    completed Authentication

---
+++ src/controllers/authController.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+
+    res.status(201).json({ success: true, user: user });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user)
+      return res.status(404).json({ status: false, message: 'No user found' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ status: false, message: 'Invalid credential' });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.status(200).json({ status: true, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
