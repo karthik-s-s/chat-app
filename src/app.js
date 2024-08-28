1d25a2258924cd0cf1bfc70c93e74b3a41cafed4
Karthik Sudhakar <karthiksudhakar999@gmail.com>
2024-08-28 22:49:57 +0530

    Base Url Added

--- src/app.js
+++ src/app.js
@@ -14,6 +14,30 @@ initSocket(server);
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send(`
+      <html>
+        <head>
+          <title>Base URL Info</title>
+          <style>
+            body { font-family: Arial, sans-serif; }
+            .container { margin: 20px; }
+            h1 { color: #333; }
+            p { font-size: 16px; }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Base URL Information</h1>
+            <p><strong>Message:</strong> This is the base URL.</p>
+            <p> Use the endpoint specified in the documentation to test this.</p>
+            <p><strong>Repository:</strong> <a href="https://github.com/karthik-s-s/chat-app.git" target="_blank">https://github.com/karthik-s-s/chat-app.git</a></p>
+          </div>
+        </body>
+      </html>
+    `);
+  });
+
 app.use('/api/auth', authRoutes);
 app.use('/api', messageRoutes);
 app.use('/api', groupRoutes);
